65311c7dbcbd53f0dc45588dd5b86d891a4e3457
Yohjit Chopra <103960257+Yohjit-2910@users.noreply.github.com>
2023-02-18 15:05:04 +0530

    submission

---
+++ api/api.js
@@ -0,0 +1,105 @@
+
+
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb+srv://Yohjit:2110994798@cluster0.7o5yej8.mongodb.net/mydb', {useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+const port = 3003;
+
+const Device = require('/Users/maharaja/Desktop/SIT209 - IOT/code/SIT209/topic6/api/models/device.js'); 
+
+app.use(express.static(`${__dirname}/public/generated-docs`));
+
+app.get('/docs', (req, res) => {
+  res.sendFile(`${__dirname}/public/generated-docs/index.html`);
+});
+
+app.get('/api/test', (req, res) => {
+  res.send('The API is working!');
+});
+
+app.post('/api/devices', (req, res) => {
+    const { name, user, sensorData } = req.body;
+    const newDevice = new Device({
+      name,
+      user,
+      sensorData
+    });
+    newDevice.save(err => {
+      return err
+        ? res.send(err)
+        : res.send('successfully added device and data');
+    });
+  });
+
+
+/**
+* @api {get} /api/devices AllDevices An array of all devices
+* @apiGroup Device
+* @apiSuccessExample {json} Success-Response:
+*  [
+*    {
+*      "_id": "dsohsdohsdofhsofhosfhsofh",
+*      "name": "Mary's iPhone",
+*      "user": "mary",
+*      "sensorData": [
+*        {
+*          "ts": "1529542230",
+*          "temp": 12,
+*          "loc": {
+*            "lat": -37.84674,
+*            "lon": 145.115113
+*          }
+*        },
+*        {
+*          "ts": "1529572230",
+*          "temp": 17,
+*          "loc": {
+*            "lat": -37.850026,
+*            "lon": 145.117683
+*          }
+*        }
+*      ]
+*    }
+*  ]
+* @apiErrorExample {json} Error-Response:
+*  {
+*    "User does not exist"
+*  }
+*/
+
+
+app.get('/api/devices', (req, res) => {
+    Device.find({}, (err, devices) => {
+      if (err == true) {
+        return res.send(err);
+      } else {
+        return res.send(devices);
+      }
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+
